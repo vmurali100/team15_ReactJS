5d8e9eeea9ce3169e0c6058df2d7885751d08a4f
vmurali100 <vmurali100@users.noreply.github.com>
2023-04-01 12:45:17 +0530

    ADDING

---
+++ Javascript/JS4_DOM/users.js
@@ -0,0 +1,32 @@
+var users = [
+    {
+        "id": 1,
+        "email": "ABreiter@dolor.io",
+        "username": "SBjurback",
+        "password": "M0NRe"
+    },
+    {
+        "id": 2,
+        "email": "VMorgan@sed.io",
+        "username": "GVanhoose",
+        "password": "iPakR"
+    },
+    {
+        "id": 3,
+        "email": "PHolt@rutrum.net",
+        "username": "DGill",
+        "password": "MPZgq"
+    },
+    {
+        "id": 4,
+        "email": "HMelvin@pulvinar.gov",
+        "username": "CDemarse",
+        "password": "JPC8u"
+    },
+    {
+        "id": 5,
+        "email": "ABoudreaux@porta.com",
+        "username": "DOdle",
+        "password": "KJQ5X"
+    }
+]
