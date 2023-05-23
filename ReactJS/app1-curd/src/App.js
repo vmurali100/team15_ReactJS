c91308b904e624483300fe3199f568386a3989cf
mallelajagadeesh <mallelajagadeesh7524@gmail.com>
2023-05-23 16:28:08 +0530

    app23

---
+++ ReactJS/app1-curd/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Person from './Components/Person';
+
+function App() {
+  return (
+    <div className="App">
+  <Person/>
+    </div>
+  );
+}
+
+export default App;
