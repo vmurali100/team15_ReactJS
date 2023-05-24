e74bd50e82a2d99c3e72aef7e19aa4067abe7557
mallelajagadeesh <mallelajagadeesh7524@gmail.com>
2023-05-24 10:55:44 +0530

    app23

---
+++ ReactJS/app22-form/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Police from './CLASS COMPONENTS/Police';
+
+function App() {
+  return (
+    <div className="App">
+ <Police/>
+    </div>
+  );
+}
+
+export default App;
