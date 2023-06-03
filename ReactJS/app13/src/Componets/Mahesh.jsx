51e9071ab2b30c5d4873110235910505671f6bcf
PrinceDilli <126316691+PrinceDilli@users.noreply.github.com>
2023-06-03 15:18:24 +0530

    adding single page application

---
+++ ReactJS/app13/src/Componets/Mahesh.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Mahesh = () => {
+  return (
+    <div>
+      <h1>Welcome To The Super Start Mahesh Babu Garu</h1>
+      <img
+        src="https://c.ndtvimg.com/2023-05/mrecq67o_mahesh-babu-_625x300_31_May_23.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
