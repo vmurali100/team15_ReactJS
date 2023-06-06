0849ec35a301c58992c31b184eaa655eb9dd95af
mallelajagadeesh <mallelajagadeesh7524@gmail.com>
2023-06-06 15:12:28 +0530

    curd

--- ReactJS/Assinments/project-5/src/Components/Sudha.jsx
+++ ReactJS/Assinments/project-5/src/Components/Sudha.jsx
@@ -2,20 +2,58 @@ import React, { Component } from "react";
 import axios from "axios";
 import { SudhaData } from "./SudhaData";
 import { SudhaContextProvider } from "./SudhaContext";
+import { SudhaForm } from "./SudhaForm";
+
 export default class Sudha extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      users: {
+        albumId: "",
+        id: "",
+        title: "",
+        url: "",
+        thumbnailUrl: "",
+      },
+      isEdit: false,
+      gIndex: 0,
       user: [],
     };
   }
   handleEdit = (usr, i) => {
-    console.log(usr);
+    this.setState({ users: usr, isEdit: true, gIndex: i });
   };
   handleDelete = (usr, i) => {
     var newUsers = this.state.user.filter((usr, index) => i != index);
     this.setState({ user: newUsers });
+  };
+  Adduser = () => {
+    var alluserscopy = [...this.state.user];
+    alluserscopy.push({ ...this.state.users });
+    this.setState({ user: alluserscopy });
+    this.clearForm();
+  };
+  handleChange = (e) => {
+    var newUser = { ...this.state.users };
+    newUser[e.target.name] = e.target.value;
+    this.setState({ users: newUser });
+  };
+  updateUser = () => {
+    var alluserscopy = [...this.state.user];
+    alluserscopy[this.state.gIndex] = this.state.users;
+    this.setState({ user: alluserscopy, isEdit: false });
+    this.clearForm();
+  };
+  clearForm = () => {
+    var EmptyUser = {
+      albumId: "",
+      id: "",
+      title: "",
+      url: "",
+      thumbnailUrl: "",
+    };
+    this.setState({ users: EmptyUser });
   };
   render() {
     return (
@@ -25,8 +63,14 @@ export default class Sudha extends Component {
             data: this.state.user,
             handleEdit: this.handleEdit,
             handleDelete: this.handleDelete,
+            users: this.state.users,
+            handleChange: this.handleChange,
+            Adduser: this.Adduser,
+            updateUser: this.updateUser,
+            isEdit: this.state.isEdit,
           }}
         >
+          <SudhaForm />
           <SudhaData />
         </SudhaContextProvider>
       </div>
