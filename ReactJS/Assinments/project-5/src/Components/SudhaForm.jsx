014c51754b258cc51c8d606b897f83dd7a54f9ae
mallelajagadeesh <mallelajagadeesh7524@gmail.com>
2023-06-06 15:12:40 +0530

    curd

---
+++ ReactJS/Assinments/project-5/src/Components/SudhaForm.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { SudhaContextConsumer } from "./SudhaContext";
+
+export const SudhaForm = () => {
+  return (
+    <div>
+      <SudhaContextConsumer>
+        {(value) => {
+          return (
+          <form>
+            <label htmlFor="albumId">AlbuamId:</label>
+            <input
+              type="text"
+              name="albumId"
+              value={value.users.albumId}
+              onChange={value.handleChange}
+            />
+            <br />
+            <label htmlFor="id">Id:</label>
+            <input
+              type="text"
+              name="id"
+              value={value.users.id}
+              onChange={value.handleChange}
+            />
+            <br />
+            <label htmlFor="title">Title:</label>
+            <input
+              type="text"
+              name="title"
+              value={value.users.title}
+              onChange={value.handleChange}
+            />
+            <br />
+            <label htmlFor="url">Url:</label>
+            <input
+              type="text"
+              name="url"
+              value={value.users.url}
+              onChange={value.handleChange}
+            />
+            <br />
+            <label htmlFor="thumbnailUrl">ThumbnailUrl:</label>
+            <input
+              type="text"
+              name="thumbnailUrl"
+              value={value.users.thumbnailUrl}
+              onChange={value.handleChange}
+            />
+            <br />
+            {value.isEdit ? (
+              <button type="button" onClick={value.updateUser}>
+                UpdateUser
+              </button>
+            ) : (
+              <button type="button" onClick={value.Adduser}>
+                AddUser
+              </button>
+            )}
+          </form>
+          )
+        }}
+      </SudhaContextConsumer>
+    </div>
+  );
+};
