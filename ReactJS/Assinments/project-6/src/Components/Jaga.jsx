5211a435ee81e46b996d4091c3c0cb4274fb3c9c
mallelajagadeesh <mallelajagadeesh7524@gmail.com>
2023-06-07 12:17:09 +0530

    curd

--- ReactJS/Assinments/project-6/src/Components/Jaga.jsx
+++ ReactJS/Assinments/project-6/src/Components/Jaga.jsx
@@ -6,11 +6,11 @@ import { JagaContextProvider } from "./JagaContext";
 import JagaTable from "./JagaTable";
 import { JagaForm } from "./JagaForm";
 
-const Jaga = () => {
+export  const Jaga = () => {
   const [Users, setUsers] = useState([]);
-  const [Index,setIndex]=useState(0)
-  const [isEdit,SetisEdit]=useStat(false)
-  const [user, setuser] = useState({
+  const [Index, setIndex] = useState(0);
+  const [IsEdit, setIsEdit] = useState(false);
+  const [User, setUser] = useState({
     id: 1,
     name: "",
     username: "",
@@ -33,17 +33,51 @@ const Jaga = () => {
       bs: "",
     },
   });
-  const handlechange = (e) => {
-    const newUser = { ...user };
-    if (e.target.name.indexof("address") > -1) {
-      newUser.address[e.target.name.split(".")[1]] = e.target.value;
-    }
-    if (e.target.name.indexof("company") > -1) {
+
+const handlechange = (e) => {
+  const newUser = { ...User };
+  if (e.target.name.indexOf("address") > -1) {
+    newUser.address[e.target.name.split(".")[1]] = e.target.value;
+  } else if (e.target.name.indexOf("geo") > -1) {
+    newUser.address["geo"][e.target.name.split(".")[1]] = e.target.value;
+    if (e.target.name.indexOf("company") > -1) {
       newUser.company[e.target.name.split(".")[1]] = e.target.value;
     } else {
       newUser[e.target.name] = e.target.value;
     }
-    setuser(newUser);
+    setUsers(newUser);
+  }
+  const ClearForm = () => {
+    setUser({
+      id: 1,
+      name: "",
+      username: "",
+      email: "",
+      address: {
+        street: "",
+        suite: "",
+        city: "",
+        zipcode: "",
+        geo: {
+          lat: "",
+          lag: "",
+        },
+      },
+      phone: "",
+      website: "",
+      company: {
+        name: "",
+        catchPhrase: "",
+        bs: "",
+      },
+    });
+  };
+  const updateuser = () => {
+    const newUser = [...Users];
+    newUser[Index] = User;
+    setUsers(newUser);
+    ClearForm();
+    setIsEdit(false);
   };
 
   useEffect(() => {
@@ -56,21 +90,36 @@ const Jaga = () => {
     const Deletedusers = Users.filter((usr, index) => i != index);
     setUsers(Deletedusers);
   };
-  const handleEdit = (usr,i) => {
+  const handleEdit = (usr, i) => {
     setUsers(usr);
-    setIndex(i)
-    SetisEdit(true)
+    setIndex(i);
+    setIsEdit(true);
     // console.log(usr);
+  };
+  const Adduser = () => {
+    const newUser = [...Users];
+    newUser.push(User);
+    setUsers(newUser);
+    ClearForm();
   };
   return (
     <div className="main">
       <JagaContextProvider
-        value={{ Users, handleDelete, handleEdit, user, handlechange }}
+        value={{
+          Users,
+          handleDelete,
+          handleEdit,
+          User,
+          handlechange,
+          updateuser,
+          IsEdit,
+          Adduser,
+        }}
       >
-        <JagaTable />
         <JagaForm />
+        <JagaTable />
       </JagaContextProvider>
     </div>
   );
 };
-export default Jaga;
+};
