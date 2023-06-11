e3f2791056fa11a2d9de02c3a9cfff5a393959e7
PrinceDilli <126316691+PrinceDilli@users.noreply.github.com>
2023-06-11 10:30:49 +0530

    adding page

--- ReactJS/Assinments/project-1/src/Componet1/DilliTable.jsx
+++ ReactJS/Assinments/project-1/src/Componet1/DilliTable.jsx
@@ -1,48 +1,56 @@
-/* eslint-disable no-unreachable */
-import React from "react";
+import React, { useState } from "react";
 import { DilliContextConsumer } from "./DilliContext";
 
 export const DilliTable = () => {
+  const entriesPerPage = 5; // Number of entries to show per page
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div>
       <DilliContextConsumer>
         {(value) => {
           console.log(value);
+          const startIndex = (currentPage - 1) * entriesPerPage;
+          const endIndex = startIndex + entriesPerPage;
+          const entriesToShow = value.data.slice(startIndex, endIndex);
+
+          const totalPages = Math.ceil(value.data.length / entriesPerPage);
+          const pageNumbers = Array.from(
+            { length: totalPages },
+            (_, index) => index + 1
+          );
+
           return (
             <div>
-              <link
-                href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-                rel="stylesheet"
-                integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
-                crossorigin="anonymous"
-              ></link>
-              <table class="table table-dark table-striped">
+              <table className="table table-dark table-striped">
                 <thead style={{ background: "#212529" }}>
                   <tr>
-                    <td>UserId</td>
-                    <td>Id</td>
-                    <td>Title</td>
-                    <td>Body</td>
-                    <br />
-                    <td>Edit</td>
-
-                    <td>Delete</td>
+                    <th>UserId</th>
+                    <th>Id</th>
+                    <th>Title</th>
+                    <th>Body</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
                   </tr>
                 </thead>
                 <tbody>
-                  {value.data.map((usr, i) => (
+                  {entriesToShow.map((usr, i) => (
                     <tr key={i}>
                       <td>{usr.userId}</td>
                       <td>{usr.id}</td>
                       <td>{usr.title}</td>
                       <td>{usr.body}</td>
-                      <td>{usr.username}</td>
                       <td>
                         <button
                           onClick={() => {
                             value.handleEdit(usr, i);
                           }}
-                       id="di1" >
+                          id="di1"
+                        >
                           Edit
                         </button>
                       </td>
@@ -51,7 +59,8 @@ export const DilliTable = () => {
                           onClick={() => {
                             value.handleDelete(usr, i);
                           }}
-                       id="id2" >
+                          id="id2"
+                        >
                           Delete
                         </button>
                       </td>
@@ -59,9 +68,27 @@ export const DilliTable = () => {
                   ))}
                 </tbody>
               </table>
+              <div>
+                <ul className="pagination">
+                  {pageNumbers.map((pageNumber) => (
+                    <li
+                      key={pageNumber}
+                      className={`page-item ${
+                        pageNumber === currentPage ? "active" : ""
+                      }`}
+                    >
+                      <button
+                        className="page-link"
+                        onClick={() => handlePageChange(pageNumber)}
+                      >
+                        {pageNumber}
+                      </button>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           );
-          
         }}
       </DilliContextConsumer>
     </div>
