747fc819c8eda4e3f33c3db5a9a428d096ed2dbe
ainarivarshitha <ainarivarshitha@gmail.com>
2023-06-20 15:33:37 +0530

    ...

--- ReactJS/app17-redux-async2/src/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import UsersSlice from "./UsersSlice";
-
-export const store = configureStore({
-    reducer:{
-        users:UsersSlice
-    }
-})
--- ReactJS/app17-redux-async3/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { Userslice } from "./Slice";
-
-export const store = configureStore({
-  reducer: {
-  users:Userslice
-  },
-});
- 
